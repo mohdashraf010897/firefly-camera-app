78796112e692b87132cc2c58f5bfbce4b066c655
namarora <namarora@adobe.com>
2024-12-13 16:33:37 +0530

    added device orientation hook

--- src/components/WelcomeScreen/index.jsx
+++ src/components/WelcomeScreen/index.jsx
@@ -1,15 +1,22 @@
 // WelcomeScreen.jsx
 import React from "react";
 import "./index.css";
+import useDeviceOrientation from "../../hooks/useDeviceOrientation";
 
 const WelcomeScreen = ({
   onTakePicture,
   onUploadImage,
   //  onUploadURL
 }) => {
+
+  const orientation = useDeviceOrientation();
+  
+
   return (
     <div className="welcome-screen">
       <h1>Adobe FireflyCam</h1>
+      <h1>Current Orientation: {orientation}</h1>
+
       <h2>Welcome to the Firefly Camera App</h2>
       <div className="cta-container">
         <button className="cta-button" onClick={onTakePicture}>
