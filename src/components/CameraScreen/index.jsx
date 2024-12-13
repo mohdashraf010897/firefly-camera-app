06a05a47db5a3b866f2bccab079fdaae463bebc1
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2024-12-13 10:08:38 +0530

    feat(camera): restrict to back camera only
    - Remove camera switching functionality
    - Set facingMode to 'environment'
    - Simplify camera UI controls

--- src/components/CameraScreen/index.jsx
+++ src/components/CameraScreen/index.jsx
@@ -2,19 +2,13 @@
 import React, { useRef, useContext, useState } from "react";
 import { Camera } from "react-camera-pro";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCamera,
-  faSyncAlt,
-  faLightbulb,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCamera, faLightbulb } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
 import withLoading from "../WithLoading";
 import { ImageContext } from "../../context/ImageContext";
 
 const CameraScreen = ({ onPhotoTaken }) => {
   const camera = useRef(null);
-  const [facingMode, setFacingMode] = useState("user");
-  const [numberOfCameras, setNumberOfCameras] = useState(0);
   const [torchOn, setTorchOn] = useState(false);
   const { setImage } = useContext(ImageContext);
 
@@ -22,11 +16,6 @@ const CameraScreen = ({ onPhotoTaken }) => {
     const photo = camera.current.takePhoto();
     setImage(photo);
     onPhotoTaken(photo);
-  };
-
-  const switchCamera = () => {
-    const newFacingMode = camera.current.switchCamera();
-    setFacingMode(newFacingMode);
   };
 
   const toggleTorch = () => {
@@ -38,19 +27,9 @@ const CameraScreen = ({ onPhotoTaken }) => {
     <div className="camera-screen">
       <div className="camera-container">
         <div className="camera-viewfinder">
-          <Camera
-            ref={camera}
-            aspectRatio="cover"
-            facingMode={facingMode}
-            numberOfCamerasCallback={setNumberOfCameras}
-          />
+          <Camera ref={camera} aspectRatio="cover" facingMode="environment" />
         </div>
         <div className="button-container">
-          {numberOfCameras > 1 && (
-            <button className="switch-button" onClick={switchCamera}>
-              <FontAwesomeIcon size="2x" icon={faSyncAlt} />
-            </button>
-          )}
           <button className="capture-button" onClick={takePhoto}>
             <FontAwesomeIcon size="3x" icon={faCamera} />
           </button>
