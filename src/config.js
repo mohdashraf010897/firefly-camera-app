fa309ed73b11f576a200af08931a6247e40ae37b
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2024-12-13 10:08:35 +0530

    config: enable URL upload feature by setting USE_DIRECT_CAMERA to false

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // src/config.js
-export const USE_DIRECT_CAMERA = true; // Set to true for direct camera UX, false for upload options UX
+export const USE_DIRECT_CAMERA = false; // Set to false to enable upload options UX
